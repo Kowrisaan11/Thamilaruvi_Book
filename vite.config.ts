4b4f7a418eab71070949def6d8ad5240557c60c7
Kowrisaan11 <kowrisaan2000@gmail.com>
2025-10-05 10:16:39 +0530

    deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Thamilaruvi_Book/", // 👈 VERY IMPORTANT for GitHub Pages (repo name)
   server: {
     host: "::",
     port: 8080,
